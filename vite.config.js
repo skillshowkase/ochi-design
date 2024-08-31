b67523472a271c81e3809bd61139ad0c24d91a43
subh-m23 <subh.m@sascoders.com>
2024-08-31 22:23:44 +0530

    modified base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/skillshowkase/ochi-design",
+  base: "./skillshowkase/ochi-design",
   plugins: [react()],
 });
